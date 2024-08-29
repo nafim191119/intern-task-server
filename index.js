05b436a3f4ac1c32d09027d67e394e35a95b9cab
MD. Ahasanul Islam Nafim <nafim191119@gmail.com>
2024-08-30 02:34:40 +0600

    up

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://interns-task-7twk.vercel.app',
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
