ab68d3162148bbd0d139dae20c50e6c10e3a877f
MD. Ahasanul Islam Nafim <nafim191119@gmail.com>
2024-08-30 02:03:16 +0600

    deployed

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-    origin: 'https://interns-task.vercel.app/',
+    origin: 'http://localhost:5173',
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
